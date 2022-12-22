383ae94a8903fd577bc391afbd81ae246357a7ca
Seongmin Kim <87827050+key1sam@users.noreply.github.com>
2022-12-23 01:05:27 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,17 +1,8 @@
 def solution(t, p):
     answer = 0
-    wordNum = 0
-    
-    if len(p) == 1 : # p의 길이가 1일 때
-        for i in range(len(t)) :
-            if t[i] <= p :
-                answer += 1
-                
-    else : # p의 길이가 2 이상인 경우
-        wordNum = len(t) - len(p) + 1 # 발생할 수 있는 부분문자열 경우의 수
-        for i in range(wordNum) :
-            tmp = t[i:i+len(p)] 
-            if tmp <= p :
-                answer += 1
-         
+    wordNum = len(t) - len(p) + 1 # 발생할 수 있는 부분문자열 경우의 수
+    for i in range(wordNum) :
+        if t[i:i+len(p)] <= p : # t[i:i+1]로 부분문자열 생성
+            answer += 1    
     return answer
+
