cd3360381b506d6f023364889f6609edd6c6089d
Seongmin Kim <87827050+key1sam@users.noreply.github.com>
2022-12-29 21:38:37 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.53 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,16 @@
+def numOfDivisor (num) :
+    count = 0
+    for i in range(1, num+1) :
+        if num % i == 0 :
+            count += 1
+    return count
+
+def solution(left, right):
+    result = 0
+    for i in range(left, right+1) :
+        now = numOfDivisor(i)
+        if now % 2 == 0 : # 약수의 개수가 짝수이면 더하기
+            result += i
+        else :
+            result -= i
+    return result
