dd146b23957762a1afde052c5222559ce6d6c926
Seongmin Kim <87827050+key1sam@users.noreply.github.com>
2022-12-30 00:57:44 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,20 +1,19 @@
 # k는 최고점수, m은 상자 당 단위개수
-# 
 
 from collections import deque
 
 def solution(k, m, score):
     
-    score.sort(reverse = True)
+    score.sort(reverse = True) # 최대 수익을 위해 역으로 정렬
     deq = deque(score)
     answer = 0
     result = 0
     box = len(score) // m # 가능한 상자의 수
     
     for i in range(box) :
-        tmp = []
-        for j in range(m) :
-            tmp.append(deq.popleft())
-        result += min(tmp)*m    
+        appleBox = []
+        for j in range(m) : 
+            appleBox.append(deq.popleft()) # 가장 높은 수부터 box 안에 들어갈 것
+        result += min(appleBox)*m    
     
     return result
