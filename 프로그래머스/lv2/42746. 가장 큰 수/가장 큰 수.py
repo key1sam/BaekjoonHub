55d1b3fa843cbafaf56ff44f769a351c64241b61
Seongmin Kim <87827050+key1sam@users.noreply.github.com>
2023-01-03 02:22:30 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,3 +1,6 @@
+# 2023.01.03 기준, 다시 풀어보기! (문제가 많이 어려운 편..)
+# 문자열을 3번 곱하여 문자열 비교를 활용하여 계산하는 코드
+# 문자열 비교는 첫번째 글자 인덱스를 비교해 나간다.
 def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x * 3, reverse=True)
