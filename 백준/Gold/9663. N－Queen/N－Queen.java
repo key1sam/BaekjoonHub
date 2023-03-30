bb85fc7fb8f32ec1c9a6df95c107e53bff1de707
Seongmin Kim <87827050+key1sam@users.noreply.github.com>
2023-03-30 14:34:46 +0900

    [Gold IV] Title: N-Queen, Time: 4900 ms, Memory: 14912 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        N = scan.nextInt();
+        col = new int[N + 1];
+    }
+
+    static int N, ans;
+    static int[] col; //col[i] = i번째 행의 퀸이 놓여있는 위치
+
+    static boolean attackable(int r1, int c1, int r2, int c2) {
+        if (c1 == c2) return true;
+        if (r1 - c1 == r2 - c2) return true;
+        if (r1 + c1 == r2 + c2) return true;
+        return false;
+    }
+
+    static void rec_func(int row) {
+        if (row == N + 1) {
+            ans++;
+        } else {
+            for (int c = 1; c <= N; c++) { // 몇번 째 열에 둘 것인가?
+                boolean possible = true;
+                // valid check (row, c)
+                for (int i=1; i<=row-1; i++){ // 현재 놓을 위치의 윗 행들을 살펴보는 반복문
+                    // (i, col[i])
+                    if (attackable(row, c, i, col[i])){ // 윗 행의 Queen의 위치를 고려했을 때 현재 자리에 둘 수 있는가?
+                        possible = false;
+                        break;
+                    }
+                }
+                if (possible) {
+                    col[row] = c;
+                    rec_func(row + 1);
+                    col[row] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
+        rec_func(1);
+        System.out.println(ans);
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
