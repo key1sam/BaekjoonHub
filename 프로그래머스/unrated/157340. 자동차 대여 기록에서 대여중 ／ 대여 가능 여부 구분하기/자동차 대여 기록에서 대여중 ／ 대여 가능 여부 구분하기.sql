c9ed2c9bd02ec8976981712bc1beab591069a901
Seongmin Kim <87827050+key1sam@users.noreply.github.com>
2023-04-10 01:50:36 +0900

    [unrated] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -5,6 +5,9 @@
 --      THEN 결과 3
 --      ELSE 결과 4
 -- END AS 별명
+-- GROUP BY의 특징 : GROUP BY로 묶게 될 경우 각각 하나의 컬럼이 나오게되는데, 이 때 각 그룹별 가장 상단의 컬럼이 출력되었다.
+-- EX) ID : 5인 컬럼이 3개가 존재할 때, ID로 GROUPING을 할 경우 컬럼 3개 중 가장 상단의 컬럼이 출력된다.
+-- 이 때, MAX, MIN이 존재할 경우 해당하는 값을 포함하는 컬럼으로 GROUP BY의 대표값으로 나타난다.
 SELECT CAR_ID, MAX(CASE WHEN '2022-10-16' BETWEEN  DATE_FORMAT(START_DATE, '%Y-%m-%d') AND  DATE_FORMAT(END_DATE, '%Y-%m-%d') THEN '대여중'
 ELSE '대여 가능' END) AS AVAILABILITY
 FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
