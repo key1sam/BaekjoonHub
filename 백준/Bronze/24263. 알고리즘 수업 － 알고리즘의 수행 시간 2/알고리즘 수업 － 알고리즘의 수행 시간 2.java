b155c2ec5f2f054af42050690e14ef81f07da172
Seongmin Kim <87827050+key1sam@users.noreply.github.com>
2023-05-10 16:29:03 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.valueOf(br.readLine());
+        System.out.println(num);
+        System.out.println(1);
+    }
+}
