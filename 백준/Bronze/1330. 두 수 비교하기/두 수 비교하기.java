bdf85589b9828d3fce76831460e37ccf4c1fa8c8
Seongmin Kim <87827050+key1sam@users.noreply.github.com>
2023-07-11 17:19:27 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        
+        if (num1 > num2) {
+            System.out.print('>');
+        }
+        else if (num1 < num2) {
+            System.out.print('<');
+        }
+        else {
+            System.out.print("==");
+        }
+    }
+}
